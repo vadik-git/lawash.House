23bf876cda3561e29c682ab1e9ba3ea844490488
HelloFront <savchik.vl@gmail.com>
2022-05-10 19:35:45 +0300

    Add: Configuration lawash model in the backend

--- server/src/middleware/lawash.middleware/index.ts
+++ server/src/middleware/lawash.middleware/index.ts
@@ -6,10 +6,12 @@ import { validationErrorMessage } from "../../utils/createErrorMessages";
 const validateLawashBody: Schema = Joi.object({
   _id: Joi.string(),
   title: Joi.string().required().min(1).max(40).messages(validationErrorMessage('string', 'title')),
-  description: Joi.string().required().min(1).messages(validationErrorMessage('string', 'description')),
   date: Joi.date().required().messages(validationErrorMessage('date', 'date')),
   isActive: Joi.boolean().required().messages(validationErrorMessage('boolean', 'isActive')),
-  price: Joi.number().required().messages(validationErrorMessage('number', 'price'))
+  price: Joi.number().required().messages(validationErrorMessage('number', 'price')),
+  image: Joi.string().required().messages(validationErrorMessage('string', 'image')),
+  ingredients: Joi.string().required().messages(validationErrorMessage('string', 'ingredients')),
+  size: Joi.string().required().messages(validationErrorMessage('string', 'size')),
 });
 
 export const ValidateBodyReq: RequestHandler = async(req, res, next) => {
